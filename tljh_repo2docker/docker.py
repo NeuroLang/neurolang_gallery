60452c313504804f4662486188eb77ef73b44883
Jonas Renault <jonas.renault@inria.fr>
2021-05-28 15:08:00 +0200

    Adding option to build docker images using the gallery.yaml file

--- tljh_repo2docker/docker.py
+++ tljh_repo2docker/docker.py
@@ -1,11 +1,12 @@
 import json
 import subprocess
 import sys
-import click
-
 from urllib.parse import urlparse
 
+import click
+import yaml
 from aiodocker import Docker
+from pkg_resources import resource_stream
 
 
 async def list_images():
@@ -88,7 +89,7 @@ def get_repo2docker_cmd(repo, ref, name="", memory=None, cpu=None):
     ]
     cmd = [
         sys.executable,
-        '-m',
+        "-m",
         "repo2docker",
         "--ref",
         ref,
@@ -154,7 +155,7 @@ async def build_image(
 
 
 @click.command()
-@click.argument("repo")
+@click.option("--repo", help="The path for the repo to clone")
 @click.option("--ref", default="master", help="The branch/ref for the repo")
 @click.option(
     "--name", default="", help="A unique name for the docker image generated"
@@ -167,7 +168,32 @@ async def build_image(
     "--cpu",
     help="nb cpus to allocate when starting a container with this image",
 )
-def build_docker_image(repo, ref, name, memory=None, cpu=None):
-    cmd, _ = get_repo2docker_cmd(repo, ref, name, memory, cpu)
-    subprocess.run(cmd)
+@click.option(
+    "-f",
+    "--file",
+    is_flag=True,
+    help="build the images specified in the gallery.yaml file.",
+)
+def build_docker_image(repo, ref, name, memory=None, cpu=None, file=False):
+    if file:
+        with resource_stream(__name__, "gallery.yaml") as f:
+            gallery = yaml.safe_load(f)
+        for name, infos in gallery.get("images", {}).items():
+            click.echo(f"Building image {name} with options {infos}.")
+            cmd, _ = get_repo2docker_cmd(name=name, **infos)
+            subprocess.run(cmd)
+    elif repo is not None:
+        click.echo(
+            "Building image {} with options {}".format(
+                name, {"ref": ref, "name": name, "memory": memory, "cpu": cpu}
+            )
+        )
+        cmd, _ = get_repo2docker_cmd(repo, ref, name, memory, cpu)
+        subprocess.run(cmd)
+    else:
+        click.echo(
+            "You must provide either a repo url (using the --repo option) or choose to "
+            "build images listed in the gallery.yaml file using the --file option.\n"
+            "Use build-image --help for usage details."
+        )
 
